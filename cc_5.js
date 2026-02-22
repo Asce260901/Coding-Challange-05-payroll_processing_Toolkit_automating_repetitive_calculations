30bcc0a427d765fc7dbd8b0330aea6533d7770bd
Asce260901 <ajm55@usf.edu>
2026-02-22 01:02:42 -0500

    Taxes and process payroll functions created and tested

--- cc_5.js
+++ cc_5.js
@@ -6,16 +6,38 @@ let employees = [
     {eName: "Jason", hourlyRate: 15.50, hoursWorked: 25}
 ];
 
+// Base up to 40 hours
 function calculateBasePay(rate, hours) {
     const basePay = Math.min(hours,40);
     return basePay * rate;
 };
-console.log(`${calculateBasePay(15.50,54)}`)
+//console.log(`${calculateBasePay(15.50,54)}`)
 
+// Overtime
 function calculateOverTimePay(rate, hours) {
     const overTime = Math.max(hours - 40, 0);
     return overTime * rate * 1.5;
 };
-console.log(`${calculateOverTimePay(20, 62)}`)
+//console.log(`${calculateOverTimePay(20, 62)}`)
 
+// Taxes
+function calculateTaxes(grosspay) {
+    return grosspay * 0.15; //15% 
+}
+//console.log(`${calculateTaxes(1000)}`)
 
+// Payroll one employee
+function processPayroll(employee) {
+    let basePay = calculateBasePay(employee.hourlyRate, employee.hoursWorked);
+    let overTime = calculateOverTimePay(employee.hourlyRate, employee.hoursWorked);
+    let grossPay = basePay + overTime;
+    let finalPay = grossPay - calculateTaxes(grossPay);
+    return {
+        name: employee.eName,
+        basePay: basePay.toFixed(2),
+        overTime: overTime.toFixed(2),
+        grossPay: grossPay.toFixed(2),
+        finalPay: finalPay.toFixed(2)
+    };
+};
+//console.log(processPayroll(employees[3]))
